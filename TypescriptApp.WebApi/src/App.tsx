533fcf65bd335ccbab906dd0638b19ad32c2e401
Sergey <svkalashnickov@gmail.com>
2020-01-30 22:12:19 +0300

    Added sample react-typescript app

---
+++ TypescriptApp.WebApi/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.less';
+import { MainRouter } from './mainRouter';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
