51c8d7827720f0e2bcb48c788c8e09892b799a1f
Sergey <svkalashnickov@gmail.com>
2020-02-02 14:37:41 +0300

    Added posts context

---
+++ TypescriptApp.Logics/PostsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TypescriptApp.DAL;
+using TypescriptApp.DAL.Models;
+
+namespace TypescriptApp.Logics
+{
+	public class PostsContext : BaseContext
+	{
+		public DbSet<Post> Posts { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
