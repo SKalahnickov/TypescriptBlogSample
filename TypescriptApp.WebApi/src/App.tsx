8f06ea24e42ae150bb9c3a63b1551f370f43dbd3
Sergey <svkalashnickov@gmail.com>
2020-02-02 14:39:44 +0300

    [updated styles]

--- TypescriptApp.WebApi/src/App.tsx
+++ TypescriptApp.WebApi/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.less';
+import './App.scss';
 import { MainRouter } from './mainRouter';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='content-container'>
         <MainRouter />
-      </header>
+      </div>
     </div>
   );
 }
