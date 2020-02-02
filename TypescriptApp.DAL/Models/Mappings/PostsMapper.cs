e656061c17edd38e67424c7c021d7a3a4608a291
Sergey <svkalashnickov@gmail.com>
2020-02-02 14:37:21 +0300

    Added mappers

---
+++ TypescriptApp.DAL/Models/Mappings/PostsMapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypescriptApp.DAL.Models.Mappings
+{
+	public class PostsMapper : BaseMapper<Post>
+	{
+		public override void Map(EntityTypeBuilder<Post> entity)
+		{
+			entity
+				.ToTable("Posts")
+				.HasKey(x => x.Id);
+
+			entity
+				.Property(x => x.Id)
+				.ValueGeneratedOnAdd()
+				.UseSerialColumn();
+
+			entity
+				.HasOne(x => x.User)
+				.WithMany(x => x.Posts)
+				.HasForeignKey(x => x.UserId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+		}
+	}
+}
---
+++ TypescriptApp.DAL/Models/Mappings/UsersMapper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypescriptApp.DAL.Models.Mappings
+{
+	public class UsersMapper : BaseMapper<User>
+	{
+		public override void Map(EntityTypeBuilder<User> entity)
+		{
+			entity
+				.ToTable("Users")
+				.HasKey(x => x.Id);
+
+			entity
+				.Property(x => x.Id)
+				.ValueGeneratedOnAdd()
+				.UseSerialColumn();
+		}
+	}
+}
