0db9a61985070e45dd9eef3dc2f8a2928248924c
Sergey <svkalashnickov@gmail.com>
2020-02-02 14:36:57 +0300

    [EF] Added initial migration

---
+++ TypescriptApp.DAL/Migrations/20200202085839_initial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TypescriptApp.DAL.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
