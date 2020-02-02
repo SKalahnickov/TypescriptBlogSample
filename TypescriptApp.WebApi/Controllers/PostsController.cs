88f883db68374abdf75d3c6f595ab812dfc11270
Sergey <svkalashnickov@gmail.com>
2020-02-02 14:40:07 +0300

    Added posts controller

---
+++ TypescriptApp.WebApi/Controllers/PostsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TypescriptApp.Logics;
+using TypescriptApp.Logics.Models;
+
+namespace TypescriptApp.WebApi.Controllers
+{
+    [Route("api/posts")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+
+        [HttpGet("list")]
+        public async Task<IReadOnlyCollection<PostModel>> GetPosts()
+        {
+            var @operator = new PostsOperator();
+            return await @operator.GetPostsAsync()
+                .ConfigureAwait(false);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<PostModel> GetPost([FromRoute] int id)
+        {
+            var @operator = new PostsOperator();
+            return await @operator.GetPostAsync(id)
+                .ConfigureAwait(false);
+        }
+
+        [HttpPost]
+        public async Task SavePost([FromBody] PostModel post)
+        {
+            var @operator = new PostsOperator();
+            await @operator.SavePostAsync(post)
+                .ConfigureAwait(false);
+        }
+    }
+}
