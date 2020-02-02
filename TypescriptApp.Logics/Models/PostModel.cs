b636316fd2d54212bd4d889a6fc789cb7e7bc95a
Sergey <svkalashnickov@gmail.com>
2020-02-02 14:38:02 +0300

    Added post model

---
+++ TypescriptApp.Logics/Models/PostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypescriptApp.Logics.Models
+{
+	public class PostModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Category { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public string CreatedBy { get; set; }
+		public string Text { get; set; }
+	}
+}
