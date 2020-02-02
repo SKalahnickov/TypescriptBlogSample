4f62160eb2b914eadfb9ff27e72533152e2e40c7
Sergey <svkalashnickov@gmail.com>
2020-02-02 14:37:12 +0300

    Added models

---
+++ TypescriptApp.DAL/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypescriptApp.DAL.Models
+{
+	public class Post
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public int UserId { get; set; }
+		public string Category { get; set; }
+		public string Text { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public virtual User User { get; set; }
+	}
+}
---
+++ TypescriptApp.DAL/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypescriptApp.DAL.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public virtual List<Post> Posts { get; set; } = new List<Post>();
+	}
+}
